6eadd0cb448a0f6f4258abe7c071201ed6dbb219
mnaofficial <mna.official92@gmail.com>
2022-03-19 01:08:28 +0600

    file add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Menubar from './Components/Menubar/Menubar';
 
 function App() {
   return (
     <div className="App">
+      <Menubar></Menubar>
       <h1 className='text-success'>MNA Fakestore</h1>
       <h6 className='text-danger'>An Online Shop</h6>
     </div>
---
+++ src/Components/Menubar/Menubar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menubar = () => {
+    return (
+        <div>
+            <h1>Shopping</h1>
+        </div>
+    );
+};
+
+export default Menubar;
